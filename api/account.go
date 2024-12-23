f122a72fd85cfc9798dbaf5aba9e33ceafe03296
Milen Todev <todevcode@gmail.com>
2024-12-23 13:52:05 +0200

    feat: add update account endpoint to modify account balance

--- api/account.go
+++ api/account.go
@@ -80,3 +80,27 @@ func (server *Server) listAccount(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, account)
 }
+
+type updateAccountRequest struct {
+	ID      int64 `json:"id" binding:"required,min=1"`
+	Balance int64 `json:"balance" binding:"required,min=0"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var req updateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.UpdateAccountParams{
+		ID:      req.ID,
+		Balance: req.Balance,
+	}
+	updatedAccount, err := server.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, updatedAccount)
+
+}
--- api/server.go
+++ api/server.go
@@ -19,6 +19,7 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccount)
+	router.PATCH("/accounts/", server.updateAccount)
 
 	server.router = router
 	return server
